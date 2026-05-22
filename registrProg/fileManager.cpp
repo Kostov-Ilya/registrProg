e8bfaeaff370594d96a56520bbbd41dc54fe5cb6
Brian <kostov.odessa21@gmail.com>
2026-05-22 17:53:44 +0300

    refactor: simplify file IO streams via fscanf_s

--- registrProg/fileManager.cpp
+++ registrProg/fileManager.cpp
@@ -1,20 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
-#pragma region files
 #include "includes.h"
 #include "struct.h"
 #include "prototypes.h"
 #include "fileManager.h"
-#pragma endregion files
 
-typedef unsigned int unInt;
-
-#pragma region saveAndLoadOper
+#pragma region fileFunctions
 void saveToFiles(const ResidentList& resList, const ApartmentList& aptList) {
     FILE* fRes = nullptr;
     if (fopen_s(&fRes, "residents.txt", "w") == 0 && fRes != nullptr) {
-        fprintf_s(fRes, "%d\n", resList.count);
-        for (int i = 0; i < resList.count; i++) {
-            fprintf_s(fRes, "%d;%s\n", resList.data[i].id, resList.data[i].fullName);
+        fprintf_s(fRes, "%llu\n", resList.count);
+        for (size_t i = 0; i < resList.count; i++) {
+            fprintf_s(fRes, "%llu;%s\n", resList.data[i].id, resList.data[i].fullName);
         }
         fclose(fRes);
     }
@@ -23,11 +19,16 @@ void saveToFiles(const ResidentList& resList, const ApartmentList& aptList) {
     }
     FILE* fApt = nullptr;
     if (fopen_s(&fApt, "apartments.txt", "w") == 0 && fApt != nullptr) {
-        fprintf_s(fApt, "%d\n", aptList.count);
-        for (int i = 0; i < aptList.count; i++) {
-            fprintf_s(fApt, "%d %d %d\n", aptList.data[i].number, aptList.data[i].floor, aptList.data[i].roomsCount);
-            for (int j = 0; j < aptList.data[i].residentCount; j++) {
-                fprintf_s(fApt, "%d ", aptList.data[i].residentIds[j]);
+        fprintf_s(fApt, "%llu\n", aptList.count);
+        for (size_t i = 0; i < aptList.count; i++) {
+            fprintf_s(fApt, "%llu %llu %llu %llu\n",
+                aptList.data[i].number,
+                aptList.data[i].floor,
+                aptList.data[i].roomsCount,
+                aptList.data[i].residentCount);
+
+            for (size_t j = 0; j < aptList.data[i].residentCount; j++) {
+                fprintf_s(fApt, "%llu ", aptList.data[i].residentIds[j]);
             }
             fprintf_s(fApt, "\n");
         }
@@ -39,66 +40,47 @@ void saveToFiles(const ResidentList& resList, const ApartmentList& aptList) {
 }
 
 void loadFromFiles(ResidentList& resList, ApartmentList& aptList) {
-    char buffer[512];
     FILE* fRes = nullptr;
     if (fopen_s(&fRes, "residents.txt", "r") == 0 && fRes != nullptr) {
-        if (fgets(buffer, sizeof(buffer), fRes) != nullptr) {
-            sscanf_s(buffer, "%d", &resList.count);
-        }
-        if (resList.count > resList.capacity) {
-            delete[] resList.data;
-            resList.data = new Resident[resList.count];
-            resList.capacity = resList.count;
-        }
-        for (int i = 0; i < resList.count; i++) {
-            if (fgets(buffer, sizeof(buffer), fRes) != nullptr) {
-                sscanf_s(buffer, "%d; %[^\n]", &resList.data[i].id,
-                    resList.data[i].fullName, (unInt)sizeof(resList.data[i].fullName));
+        if (fscanf_s(fRes, "%llu\n", &resList.count) == 1) {
+            if (resList.count > resList.capacity) {
+                delete[] resList.data;
+                resList.data = new Resident[resList.count];
+                resList.capacity = resList.count;
+            }
+            for (size_t i = 0; i < resList.count; i++) {
+                fscanf_s(fRes, "%llu;", &resList.data[i].id);
+                size_t j = 0;
+                char ch;
+                while (fscanf_s(fRes, "%c", &ch, 1) == 1 && ch != '\n' && j < nameBuffer - 1) {
+                    resList.data[i].fullName[j++] = ch;
+                }
+                resList.data[i].fullName[j] = '\0';
             }
         }
         fclose(fRes);
     }
     FILE* fApt = nullptr;
     if (fopen_s(&fApt, "apartments.txt", "r") == 0 && fApt != nullptr) {
-        if (fgets(buffer, sizeof(buffer), fApt) != nullptr) {
-            sscanf_s(buffer, "%d", &aptList.count);
-        }
-        if (aptList.count > aptList.capacity) {
-            delete[] aptList.data;
-            aptList.data = new Apartment[aptList.count];
-            aptList.capacity = aptList.count;
-        }
-        for (int i = 0; i < aptList.count; i++) {
-            bool gotApt = false;
-            while (fgets(buffer, sizeof(buffer), fApt) != nullptr) {
-                if (buffer[0] != '\n' && buffer[0] != '\r') {
-                    sscanf_s(buffer, "%d %d %d",
-                        &aptList.data[i].number,
-                        &aptList.data[i].floor,
-                        &aptList.data[i].roomsCount);
-                    gotApt = true;
-                    break;
-                }
+        if (fscanf_s(fApt, "%llu\n", &aptList.count) == 1) {
+            if (aptList.count > aptList.capacity) {
+                delete[] aptList.data;
+                aptList.data = new Apartment[aptList.count];
+                aptList.capacity = aptList.count;
             }
-            if (!gotApt) {
-                break;
-            }
-            aptList.data[i].residentCount = 0;
-            if (fgets(buffer, sizeof(buffer), fApt) != nullptr) {
-                char* token = nullptr;
-                char* nextToken = nullptr;
-                token = strtok_s(buffer, " \n\r", &nextToken);
-                while (token != nullptr && aptList.data[i].residentCount < maxPerApt) {
-                    int id = atoi(token);
-                    if (id > 0) {
-                        aptList.data[i].residentIds[aptList.data[i].residentCount] = id;
-                        aptList.data[i].residentCount++;
-                    }
-                    token = strtok_s(nullptr, " \n\r", &nextToken);
+            for (size_t i = 0; i < aptList.count; i++) {
+                fscanf_s(fApt, "%llu %llu %llu %llu\n",
+                    &aptList.data[i].number,
+                    &aptList.data[i].floor,
+                    &aptList.data[i].roomsCount,
+                    &aptList.data[i].residentCount);
+
+                for (size_t j = 0; j < aptList.data[i].residentCount; j++) {
+                    fscanf_s(fApt, "%llu", &aptList.data[i].residentIds[j]);
                 }
             }
         }
         fclose(fApt);
     }
 }
-#pragma endregion saveAndLoadOper
+#pragma endregion fileFunctions
