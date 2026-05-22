a7e84f8ff42bfee198b02dea64e96303a8706c93
Brian <kostov.odessa21@gmail.com>
2026-05-22 17:43:22 +0300

    Correct prototypes functions

--- registrProg/prototypes.h
+++ registrProg/prototypes.h
@@ -1,57 +1,34 @@
 #pragma once
 #include "struct.h"
 
-#pragma region templatesFunc
-template <typename T>
-int findPerById(const T* const array, int count, int targetId) {
-    for (int i = 0; i < count; i++) {
-        if (array[i].id == targetId) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-template <typename T>
-bool growArray(T*& arr, int* const capacity) {
-    int newCap = (*capacity) * 2;
-    T* newArr = new (std::nothrow) T[newCap];
-    if (newArr == nullptr) {
-        printf("Ошибка: Не удалось выделить память!\n");
-        return false;
-    }
-    for (int i = 0; i < *capacity; i++) {
-        newArr[i] = arr[i];
-    }
-    delete[] arr;
-    arr = newArr;
-    *capacity = newCap;
-    return true;
-}
-#pragma endregion templatesFunc
+#pragma region utilityFunctions
+size_t findResidentIndexById(const Resident* const array, size_t count, size_t targetId);
+void growResidents(Resident*& arr, size_t* const capacity);
+void growApartments(Apartment*& arr, size_t* const capacity);
+#pragma endregion utilityFunctions
 
 #pragma region stringFunct
 int strCmp(const char* s1, const char* s2);
-void copyString(char* const dest, const char* src, int bufferSize);
-void clearString(char* const str, int size);
-int stringLength(const char* str);
+void copyString(char* const dest, const char* src, size_t bufferSize);
+void clearString(char* const str, size_t size);
+size_t stringLength(const char* str);
 #pragma endregion stringFunct
 
 #pragma region resManagement
 void addResident(ResidentList& resList);
-void deleteResident(ResidentList& resList, int rId);
+void deleteResident(ResidentList& resList, size_t rId);
 void printAllResidents(const ResidentList& resList);
 #pragma endregion resManagement
 
 #pragma region aparManagement
 void addApartment(ApartmentList& aptList);
-void deleteApartment(ApartmentList& aptList, int aptNumber);
-void linkResidentToApartment(ApartmentList& aptList, int aptNumber, int resId);
-void unlinkResidentFromApartment(ApartmentList& aptList, int aptNumber, int resId);
+void deleteApartment(ApartmentList& aptList, size_t aptNumber);
+void linkResidentToApartment(ApartmentList& aptList, size_t aptNumber, size_t resId);
+void unlinkResidentFromApartment(ApartmentList& aptList, size_t aptNumber, size_t resId);
 void printAllApartments(const ApartmentList& aptList);
-void printResidentsInApartment(int aptNumber, const ApartmentList& aptList, const ResidentList& resList);
-void printApartmentsByFloor(const ApartmentList& aptList, int floor);
-void printApartmentsByType(const ApartmentList& aptList, int rooms);
+void printResidentsInApartment(size_t aptNumber, const ApartmentList& aptList, const ResidentList& resList);
+void printApartmentsByFloor(const ApartmentList& aptList, size_t floor);
+void printApartmentsByType(const ApartmentList& aptList, size_t rooms);
 #pragma endregion aparManagement
 
 #pragma region SystemFunctions
